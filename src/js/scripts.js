339cb3f30a1ffc65f97d19008be70b760dbf5a60
Mario Yepes <marioy47@gmail.com>
2023-03-06 22:39:31 +0700

    Fixing linting issues in the script.js file

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,16 +4,11 @@
 		'[data-selector-type="display-content"]'
 	);
 	const [...contents] = document.querySelectorAll('[data-content-id]');
-	tabs.map((tab) => {
+	tabs.forEach((tab) => {
 		tab.addEventListener('click', (ev) => {
 			ev.preventDefault();
 			const itemToDisplay = ev.target.dataset.selectorTarget;
 			contents.forEach((content) => {
-				console.log(
-					tab.dataset.selectorTarget,
-					' 0 ',
-					content.dataset.contentId
-				);
 				if (itemToDisplay === content.dataset.contentId) {
 					content.style.setProperty('--display', 1);
 				} else {
