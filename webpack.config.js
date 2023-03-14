6697ed03bb844d7cb1f56e3c5ec4cda7d31b3baa
Mario Yepes <marioy47@gmail.com>
2023-03-14 11:21:33 +0700

    Adding rtlcss webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -8,29 +8,30 @@ const path = require('path');
 module.exports = {
 	...webpackWpConfig,
 	entry: {
-		// Instead of one mamin.js file, we have 2 stylesheets and 2 javascripts.
+		// Declare multiple outputs for the various files.
 		scripts: './src/js/scripts.js',
 		'admin-scripts': './src/js/admin-scripts.js',
 		styles: './src/scss/styles.scss',
 		'admin-styles': './src/scss/admin-styles.scss',
 	},
 	output: {
-		// Change the output folder from build to dist
+		// Change the output folder from build/ to dist/
 		path: path.resolve(__dirname, 'dist'),
 	},
 	plugins: [
-		// Remove the empty JS files that webpack creates after compiling SCSS files
-		new FixStyleOnlyEntriesPlugin(),
-
-		// remove the DependencyExtractionWebpackPlugin so the main.assets.php file desn't gets generated.
+		// Original plugins EXCEPT the generator of "main.assets.php" files.
 		...webpackWpConfig.plugins.filter(
 			(plugin) =>
 				plugin.constructor.name !== 'DependencyExtractionWebpackPlugin'
 		),
 
+		// Remove the empty JS files that WebPack creates after compiling SCSS files.
+		new FixStyleOnlyEntriesPlugin(),
+
+		// Support for RTL languages.
 		new RtlCssPlugin('[name]-rtl.css'),
 
-		// Copy HTML files to the dist/ directory
+		// Copy HTML files to the dist/ directory.
 		new CopyPlugin({
 			patterns: [
 				{
@@ -40,7 +41,7 @@ module.exports = {
 			],
 		}),
 
-		// Add browsersync for automatic browser reload
+		// Add BrowserSync for automatic browser reload and CSS injection upon changes.
 		new BrowserSyncPlugin({
 			host: 'localhost',
 			port: 3000,
